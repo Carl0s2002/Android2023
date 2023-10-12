93a26cc18870139371a2f03c52dd5c2c09ca7289
Carl0s2002 <99868670+Carl0s2002@users.noreply.github.com>
2023-10-12 16:38:17 +0300

    Labor 2 megoldva

--- kotlin-exercises/labor-02/src/main/Date.kt
+++ kotlin-exercises/labor-02/src/main/Date.kt
@@ -6,4 +6,19 @@ data class Date(
     var year: Int = Calendar.getInstance().get(Calendar.YEAR) ,
     var month: Int = Calendar.getInstance().get(Calendar.MONTH) ,
     var day: Int = Calendar.getInstance().get(Calendar.DAY_OF_MONTH) ,
-)
+) : Comparable<Date> {
+    override fun compareTo(other: Date): Int {
+        // Compare years
+        if (year != other.year) {
+            return year - other.year
+        }
+
+        // If years are equal, compare months
+        if (month != other.month) {
+            return month - other.month
+        }
+
+        // If months are equal, compare days
+        return day - other.day
+    }
+}
--- kotlin-exercises/labor-02/src/main/Main.kt
+++ kotlin-exercises/labor-02/src/main/Main.kt
@@ -3,6 +3,7 @@ package main
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Random
 
 enum class DictionaryType {
     ARRAY_LIST ,
@@ -38,8 +39,47 @@ fun main(args: Array<String>) {
     val date = Date() ;
     println("Date: ${date.year}.${date.month}.${date.day}") ;
     println("Is it a leap year? ${date.isLeapYear()}" ) ;
-    
+    println("Is it a valid date? ${date.isValidDate()}") ;
 
+    val validDates:MutableList<Date> = mutableListOf<Date>()  ;
+    val random = Random() ;
+    while ( validDates.size < 10 ) {
+        val year = random.nextInt(2200) ;
+        val month = random.nextInt(12) + 1 ;
+        val day = random.nextInt(31)+1 ;
+        val validDate = Date(year , month , day) ;
+        if ( validDate.isValidDate() ) {
+            validDates.add(validDate) ;
+        }
+    }
+
+    println("Valid dates generated: ${validDates}") ;
+
+    validDates.sort() ;
+    println("Sorted valid dates:")
+    validDates.forEach { date ->
+        println("${date.year}.${date.month}.${date.day}")
+    }
+
+    val reversedDates = validDates.reversed() ;
+    println("Reversed valid dates:") ;
+    reversedDates.forEach { date ->
+        println("${date.year}.${date.month}.${date.day}") ;
+    }
+
+    val customDateComparator = Comparator<Date> { date1, date2 ->
+        if ( date1.day != date2.day ) {
+            date1.day - date2.day ;
+        }
+        else {
+            date1.year - date2.year
+        }
+    }
+
+    validDates.sortWith(customDateComparator) ;
+    println("Custom sort: ") ;
+    validDates.forEach { date ->
+        println("${date.year}.${date.month}.${date.day}") ; }
 
 }
 
@@ -51,3 +91,6 @@ fun longestString( list:List<String> ) = list.max() ;
 
 fun Date.isLeapYear():Boolean = year % 4 == 0 ;
 
+fun Date.isValidDate(): Boolean {
+    return year >= 0 && year < 2024 && month in 1..12 && day in 1..31
+}
