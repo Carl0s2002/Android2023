05aa60c74f47d85f780c3a5a93e3aa54cbe70704
Carl0s2002 <99868670+Carl0s2002@users.noreply.github.com>
2023-10-27 09:36:14 +0300

    Labor 5
    Navigation between Splash and Main Activity and also printed it out their lifecycle methods

--- android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
@@ -3,15 +3,44 @@ package com.tasty.recipesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tasty.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     companion object{
-        private val TAG = "MainActivity" ;
+        private val TAG = MainActivity::class.java.simpleName ;
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG , "onCreate: MainActivity created.") ;
+        val view = ActivityMainBinding.inflate(layoutInflater) ;
+        setContentView(view.root) ;
+        val name = intent.getStringExtra("name") ;
+        view.textGreeting.text = "Hello $name" ;
+    }
+    override fun onStart(){
+        super.onStart() ;
+        Log.d(TAG, "onStart: MainActivity started") ;
+    }
+
+    override fun onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: MainActivity resuming") ;
+    }
+
+    override fun onPause() {
+        super.onPause() ;
+        Log.d(TAG, "onPause: MainActivity paused") ;
+    }
+
+    override fun onStop() {
+        super.onStop() ;
+        Log.d(TAG, "onStop: MainActivity stopped") ;
+    }
+
+    override fun onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy MainActivity destroyed");
     }
 }
--- android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -1,17 +1,61 @@
 package com.tasty.recipesapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.tasty.recipesapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivitySplashBinding
     companion object{
-        private val TAG = "Splash Activity" ;
+        private val TAG = SplashActivity::class.java.simpleName ;
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        Log.d(TAG, "onCreate: SplashActivity created.") ;
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG , "onCreate: SplashActivity") ;
+        val textInput = binding.textInput ;
+        binding.buttonNavigate.setOnClickListener {
+            val intent = Intent(this@SplashActivity,MainActivity::class.java) ;
+            intent.putExtra("name" , textInput.text.toString()) ;
+            startActivity(intent) ;
+        }
+
+        }
+    override fun onStart(){
+        super.onStart() ;
+        Log.d(TAG , "onStart: SplashActivity started") ;
     }
-}
+
+    override fun onResume() {
+        super.onResume();
+        Log.d(TAG , "onResume: SplashActivity resuming") ;
+    }
+
+    override fun onPause() {
+        super.onPause() ;
+        Log.d(TAG , "onPause: SplashActivity paused") ;
+    }
+
+    override fun onStop() {
+        super.onStop() ;
+        Log.d(TAG , "onStop: SplashActivity stopped") ;
+    }
+
+    override fun onDestroy() {
+        super.onDestroy();
+        Log.d(TAG , "onDestroy SplashActivity destroyed");
+    }
+
+    }
